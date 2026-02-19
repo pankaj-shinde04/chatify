afd3f3f2bd441ce136189be92ce5b74c96972713
pankaj-shinde04 <pankajshinde2434@gmail.com>
2026-02-19 13:53:26 +0530

    signup controller added

---
+++ backend/src/lib/util.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (userId,res)=>{
+    const token = jwt.sign({userId}, process.env.JWT_SECRET,{
+        expiresIn: "7d",
+    });
+
+    res.cookie("jwt",token,{
+        maxAge: 7*24*60*60*1000, // 7 days in milliseconds
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "development"? false:true, // set secure flag in development
+        sameSite: "strict", // prevent CSRF
+    });
+
+    return token;
+}
