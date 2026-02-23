bb21849fe6526168db079e0dd575e3af4f8591a3
pankaj-shinde04 <pankajshinde2434@gmail.com>
2026-02-23 17:09:14 +0530

    rate limiting implemented

--- backend/src/routes/authroute.js
+++ backend/src/routes/authroute.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import { signup, login, logout, updateProfile } from '../controllers/authcontroller.js';
 import { protectRoute } from '../middleware/auth.middleware.js'; 
+import { arcjetProtection } from '../middleware/arcjet.middleware.js';
 
 const router = express.Router();
 
-router.post("/signup", signup);
+router.use(arcjetProtection); // Apply Arcjet protection to all routes in this router
+router.post("/signup",  signup);
 router.post("/login",login );
-router.post("/logout", logout);
+router.post("/logout",logout);
 
-router.put("/updateProfile", protectRoute, updateProfile);
+router.put("/updateProfile", arcjetProtection,protectRoute, updateProfile);
 
 router.get("/check", protectRoute, (req,res)=> res.status(200).json(req.user ));
 
