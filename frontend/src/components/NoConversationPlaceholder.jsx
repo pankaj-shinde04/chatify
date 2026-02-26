ffc3176784add59dc1cf47571b7aae934f5a9541
pankaj-shinde04 <pankajshinde2434@gmail.com>
2026-02-26 18:13:05 +0530

    chat page layout setup

---
+++ frontend/src/components/NoConversationPlaceholder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoConversationPlaceholder = () => {
+  return (
+    <div>
+      <h1 className='text-white'>NoConversationPlaceholder</h1>
+    </div>
+  )
+}
+
+export default NoConversationPlaceholder;
