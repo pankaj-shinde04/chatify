d5c8c45dce8c03e21c79613dc2cdfa6c548bbe40
pankaj-shinde04 <pankajshinde2434@gmail.com>
2026-02-28 18:33:25 +0530

    Added file input and image preview to message input, implemented optimistic message sending, and updated chat store to handle optimistic messages

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useChatStore } from '../store/useChatStore'
 import {useAuthStore} from '../store/useAuthStore'
 import ChatHeader from './ChatHeader';
@@ -10,10 +10,19 @@ import MessagesLoadingSkeleton from './MessagesLoadingSkeleton';
 const ChatContainer = () => {
  const {selectedUser,getMessagesByUserId, messages,isMessagesLoading} =  useChatStore();
  const {authUser} = useAuthStore();
+ const messageEndRef = useRef(null)
 
  useEffect(()=>{
   getMessagesByUserId(selectedUser._id);
- },[selectedUser, getMessagesByUserId])
+ },[selectedUser, getMessagesByUserId]);
+
+
+ useEffect(() => {
+    if (messageEndRef.current) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+
   return (
     <>
      <ChatHeader /> 
@@ -45,6 +54,7 @@ const ChatContainer = () => {
                 </div>    
             </div>
           ))}
+          <div ref={messageEndRef} />
           </div>
       ) : isMessagesLoading ? <MessagesLoadingSkeleton/> : (
        < NoChatHistoryPlaceholder name={selectedUser.fullName} />
